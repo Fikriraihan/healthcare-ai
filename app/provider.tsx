97450a9911760929819ab2360bc7b970a0ecf5e8
Fikri Raihan <fikri.raihan4@gmail.com>
2025-12-11 19:18:44 +0700

    feat: add react query

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,9 +2,15 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <ClerkProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ClerkProvider>
+  );
 };
 
 export default Provider;
